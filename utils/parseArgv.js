7f3a11fb5de718cca70bd2bb270110107fcee557
Matthew Volk <volkmattj@gmail.com>
2019-01-05 12:02:59 -0800

    use minimist to parse CLI args

--- utils/parseArgv.js
+++ utils/parseArgv.js
@@ -1,7 +1,8 @@
 module.exports = (argv) => {
-  if (!argv[2]) return console.log("\n\x1b[31;1m%s\x1b[0m", "\u2718 No arguments found!");
+
+  argv = require('minimist')(process.argv.slice(2));
+
+  if (!argv) return console.log("\n\x1b[31;1m%s\x1b[0m", "\u2718 No arguments found!");
   console.log('\n\x1b[92;1m\u2714 arguments detected:\x1b[0m');
-  for (var i = 2; i < argv.length; i++) {
-    console.log(argv[i]);
-  }
+  console.log(argv);
 }
