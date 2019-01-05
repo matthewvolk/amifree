7c92d6c8da7592dd0138455ff423477bf7a6b3d2
Matthew Volk <volkmattj@gmail.com>
2019-01-05 14:00:45 -0800

    add debug logging for parsing cli arguments

--- utils/parseCLIArgs.js
+++ utils/parseCLIArgs.js
@@ -2,7 +2,15 @@ module.exports = (argv) => {
 
   argv = require('minimist')(process.argv.slice(2));
 
-  if (!argv) return console.log("\n\x1b[31;1m%s\x1b[0m", "\u2718 No arguments found!");
-  console.log('\n\x1b[92;1m\u2714 arguments object detected:\x1b[0m');
+  if (!argv) {
+    console.log("\n\x1b[31;1m%s\x1b[0m", "\u2718 No arguments found!")
+    return;
+  }
+  if (argv && argv._.length == 0 && Object.getOwnPropertyNames(argv).length === 1) {
+    console.log('\n\x1b[92;1m\u2714 arguments object detected \x1b[93;1mWITHOUT \x1b[92;1marguments:\x1b[0m');
+    console.log(argv);
+    return;
+  } 
+  console.log('\n\x1b[92;1m\u2714 arguments object detected with arguments:\x1b[0m');
   console.log(argv);
 }
