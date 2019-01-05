65380c9244b29bd9d5b8add1b209b06692feb4cc
Matthew Volk <volkmattj@gmail.com>
2019-01-05 12:35:45 -0800

    change verbiage of console log for clarity

--- utils/parseArgv.js
+++ utils/parseArgv.js
@@ -3,6 +3,6 @@ module.exports = (argv) => {
   argv = require('minimist')(process.argv.slice(2));
 
   if (!argv) return console.log("\n\x1b[31;1m%s\x1b[0m", "\u2718 No arguments found!");
-  console.log('\n\x1b[92;1m\u2714 arguments detected:\x1b[0m');
+  console.log('\n\x1b[92;1m\u2714 arguments object detected:\x1b[0m');
   console.log(argv);
 }
