bc24b32afcbe22a06de0d1161c0189a6f4581413
matthewvolk <volkmattj@gmail.com>
2019-01-06 18:02:26 -0800

    add todos

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const path = require('path');
 require('dotenv').config({path: path.join(__dirname, ".env")});
 
@@ -15,6 +16,17 @@ require('dotenv').config({path: path.join(__dirname, ".env")});
 module.exports = (argv) => {
   argv = require('minimist')(argv.slice(2));
   let cmd = argv._[0] || 'help'
+
+  /**
+   * TODO: If user has not yet set process.env.CALENDAR_ID, terminate the application.
+   */
+  console.log('\n\x1b[92;1m\u2714 process.env.CALENDAR_ID:\x1b[0m', process.env.CALENDAR_ID);
+  
+  /**
+   * TODO: If user has not yet set credentials.json, terminate the application.  
+   */
+  if (fs.existsSync(path.join(__dirname, "credentials.json"))) console.log('\x1b[92;1m\u2714 credentials.json\x1b[0m');
+
 
   if (argv.version || argv.v) {
     cmd = 'version'
