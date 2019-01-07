933409e459c8928050283a02be5cb7f850c5e144
matthewvolk <volkmattj@gmail.com>
2019-01-06 16:34:00 -0800

    add better documentation

--- cmds/help.js
+++ cmds/help.js
@@ -1,11 +1,16 @@
-const help = `
-  amifree [command]
+const help = `Usage: amifree [options]
 
-  commands:
-  today ...................... show availability for today
-  tomorrow.................... show availability for tomorrow
-  version, -v, --version ..... show package version
-  help, -h, --help ........... show this help menu
+Options:
+  today                    The person name.
+  tomorrow                 The person birthday or born date.
+  version, -v, --version   Comming birthdays. Pass a date in the future.
+  help, -h, --help         Use a different birthday json file path.
+
+Examples:
+  amifree today            # Displays available timeslots on your calendar for current day
+  amifree tomorrow         # Displays availabel timelines on your calendar for tomorrow 
+
+Documentation can be found at https://github.com/matthewvolk/amifree
 `
 module.exports = () => {
   console.log(help);
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { checkEnvironmentVariables } = require('./lib');
 require('dotenv').config();
 
 /**
