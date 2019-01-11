7b235b224525d21926fe46ed375cd22e01b1987d
matthewvolk <volkmattj@gmail.com>
2019-01-10 23:10:59 -0800

    change error messaging

--- cmds/error.js
+++ cmds/error.js
@@ -1,6 +1,6 @@
 module.exports = (cmd) => {
 console.log(`
-\x1b[31;1m\u2718 ERROR: "${cmd}" is not a valid command!
+\x1b[31;1m\u2718 ERROR: command not found: "${cmd}"
 `)
 require('./help')();
 }
