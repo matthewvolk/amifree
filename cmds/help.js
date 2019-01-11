b504846433ffca6e18952c59133a35689cc98949
matthewvolk <volkmattj@gmail.com>
2019-01-10 22:30:52 -0800

    message font color changes

--- cmds/help.js
+++ cmds/help.js
@@ -1,14 +1,14 @@
-const help = `Usage: amifree [options]
+const help = `\x1b[0mUsage: amifree [options]
 
 Options:
-  today                    List Google Calendar availability for today.
-  tomorrow                 List Google Calendar availability for tomorrow.
-  version, -v, --version   Show package version.
-  help, -h, --help         Display this help menu.
+  \x1b[36;1mtoday\x1b[0m                    List Google Calendar availability for today.
+  \x1b[36;1mtomorrow\x1b[0m                 List Google Calendar availability for tomorrow.
+  \x1b[36;1mversion, -v, --version\x1b[0m   Show package version.
+  \x1b[36;1mhelp, -h, --help\x1b[0m         Display this help menu.
 
 Examples:
-  amifree today            # Displays available timeslots on your calendar for current day
-  amifree tomorrow         # Displays availabel timelines on your calendar for tomorrow 
+  \x1b[36;1mamifree today\x1b[0m            # Displays available timeslots on your calendar for current day
+  \x1b[36;1mamifree tomorrow\x1b[0m         # Displays availabel timelines on your calendar for tomorrow 
 
 Documentation can be found at https://github.com/matthewvolk/amifree
 `
