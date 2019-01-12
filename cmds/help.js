2e21d8d6ab27eb60733e41a3d0142d94dd55d372
matthewvolk <volkmattj@gmail.com>
2019-01-12 08:10:18 -0800

    add to help directory

--- cmds/help.js
+++ cmds/help.js
@@ -1,8 +1,11 @@
-const help = `\x1b[0mUsage: amifree [options]
+const help = `
+\x1b[0mUsage: amifree [options]
 
 Options:
   \x1b[36;1mtoday\x1b[0m                    List Google Calendar availability for today.
   \x1b[36;1mtomorrow\x1b[0m                 List Google Calendar availability for tomorrow.
+  \x1b[36;1mconfig\x1b[0m                    One time CLI setup and configuration process.
+  \x1b[36;1minfo\x1b[0m                     Print out information about your configuration.
   \x1b[36;1mversion, -v, --version\x1b[0m   Show package version.
   \x1b[36;1mhelp, -h, --help\x1b[0m         Display this help menu.
 
