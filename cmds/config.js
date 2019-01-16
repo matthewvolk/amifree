288e1cd4fb4f2b5b8cc06184c55629e53aeaad60
matthewvolk <volkmattj@gmail.com>
2019-01-15 19:19:32 -0800

    play with formatting

--- cmds/config.js
+++ cmds/config.js
@@ -11,10 +11,14 @@ module.exports = () => {
   console.log(config);
   process.stdout.write(`\x1b[1m1. Open a web browser and navigate to: \x1b[0m`)
   console.log(`\x1b[34;4mhttps://console.developers.google.com/\x1b[0m`);
-  console.log('\x1b[1m2. From the project drop-down, choose Create a new project, enter a name for the project, click Create.\x1b[0m');
-  console.log('\x1b[1m3. Click "Library" from the navigation menu on the left and add Google Calendar API to your project.\x1b[0m');
-  console.log('\x1b[1m4. On the Credentials page, select Create credentials, then select OAuth client ID.\x1b[0m');
-  console.log('\x1b[1m5. Supply the requested information, and click Save to return to the Credentials screen.\x1b[0m');
-  console.log('\x1b[1m6. Select Other for the Application type, and enter any additional information required.\x1b[0m');
-  console.log('\x1b[1m7. Navigate back to your credentials dashboard, and download the JSON file containing your credentials.\x1b[0m');
+  console.log('\x1b[1m2. From the project drop-down, choose Create a new project.\x1b[0m');
+  console.log('\x1b[1m3. Enter a name for the project, click Create.\x1b[0m');
+  console.log('\x1b[1m4. Click "Library" from the navigation menu on the left.\x1b[0m');
+  console.log('\x1b[1m5. Add Google Calendar API to your project.\x1b[0m');
+  console.log('\x1b[1m6. On the Credentials page, select Create credentials.\x1b[0m');
+  console.log('\x1b[1m7. Select OAuth client ID.\x1b[0m');
+  console.log('\x1b[1m8. Supply the requested information.\x1b[0m');
+  console.log('\x1b[1m9. Click Save to return to the Credentials screen.\x1b[0m');
+  console.log('\x1b[1m10. Select Other for the Application type.\x1b[0m');
+  console.log('\x1b[1m11. Download the JSON file containing your credentials.\x1b[0m');
 }
