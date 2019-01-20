6b7af89200a56f934d8ccff7cdfd176e3a3fd21a
Matthew Volk <volkmattj@gmail.com>
2019-01-20 10:45:45 -0800

    add user input to config cmd

--- cmds/config.js
+++ cmds/config.js
@@ -1,3 +1,8 @@
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
 const config = `\x1b[1m
                  _  __              ___  
                 (_)/ _|            |__ \\ 
@@ -11,14 +16,17 @@ module.exports = () => {
   console.log(config);
   process.stdout.write(`\x1b[1m1. Open a web browser and navigate to: \x1b[0m`)
   console.log(`\x1b[34;4m\nhttps://console.developers.google.com/\x1b[0m`);
-  console.log('\x1b[1m2. From the project drop-down, choose Create a new project.\x1b[0m');
-  console.log('\x1b[1m3. Enter a name for the project, click Create.\x1b[0m');
-  console.log('\x1b[1m4. Click "Library" from the navigation menu on the left.\x1b[0m');
-  console.log('\x1b[1m5. Add Google Calendar API to your project.\x1b[0m');
-  console.log('\x1b[1m6. On the Credentials page, select Create credentials.\x1b[0m');
-  console.log('\x1b[1m7. Select OAuth client ID.\x1b[0m');
-  console.log('\x1b[1m8. Supply the requested information.\x1b[0m');
-  console.log('\x1b[1m9. Click Save to return to the Credentials screen.\x1b[0m');
-  console.log('\x1b[1m10. Select Other for the Application type.\x1b[0m');
-  console.log('\x1b[1m11. Download the JSON file containing your credentials.\x1b[0m');
+  process.stdout.write(`\x1b[1m2. Follow steps 1 - 3 under "Get started": \x1b[0m`)
+  console.log(`\x1b[34;4m\nhttps://support.google.com/cloud/answer/3465889?hl=en\x1b[0m`);
+  console.log('\x1b[1m3. Navigate to the Credentials page, select Create credentials.\x1b[0m');
+  console.log('\x1b[1m4. Select OAuth client ID.\x1b[0m');
+  console.log('\x1b[1m5. Supply the requested information.\x1b[0m');
+  console.log('\x1b[1m6. Click Save to return to the Credentials screen.\x1b[0m');
+  console.log('\x1b[1m7. Select Other for the Application type.\x1b[0m');
+  console.log('\x1b[1m8. Download the JSON file containing your credentials.\x1b[0m');
+
+  readline.question("\nDrag the downloaded credentials JSON file into this terminal to get the path and then press ENTER:\n", (path) => {
+    console.log(`Path: ${path}`);
+    readline.close();
+  });
 }
