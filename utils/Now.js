1700421cdd0f155ae60955929f478ee9e519eccd
Matthew Volk <volkmattj@gmail.com>
2019-01-23 07:43:06 -0800

    export day util

--- utils/Now.js
+++ utils/Now.js
@@ -5,3 +5,8 @@ const START_OF_TODAY = new Date();
       START_OF_TODAY.setHours(0,0,0,0);
 const END_OF_TODAY = new Date();
       END_OF_TODAY.setHours(23,59,59,999);  
+
+module.exports = {
+  START_OF_TODAY,
+  END_OF_TODAY
+}
