0a916da50d51c65197ec2b44133892164352640a
Matthew Volk <volkmattj@gmail.com>
2019-02-02 21:53:47 -0800

    fix conditional logic

--- index.js
+++ index.js
@@ -17,28 +17,28 @@ module.exports = (argv) => {
   argv = require('minimist')(argv.slice(2));
   let cmd = argv._[0] || 'help';
 
-  /**
-   * TODO: If user has not yet set process.env.CALENDAR_ID, terminate the application.
-   */
-  if (!process.env.CALENDAR_ID) {
-    console.log("\x1b[31;1m%s\x1b[0m", "\u2718 .env file not found! Please run 'amifree setup' to fix this error.");
-    return;
-  }
-  
-  /**
-   * TODO: If user has not yet set credentials.json, terminate the application.  
-   */
-  if (fs.existsSync(path.join(__dirname, "credentials.json"))) {
-    console.log("\x1b[31;1m%s\x1b[0m", "\u2718 credentials.json file not found! Please run 'amifree setup' to fix this error.");
-    return;
-  }
-
   if (argv.version || argv.v) {
     cmd = 'version'
   }
 
   if (argv.help || argv.h) {
     cmd = 'help'
+  }
+
+  /**
+   * TODO: If user has not yet set process.env.CALENDAR_ID, terminate the application.
+   */
+  if (!process.env.CALENDAR_ID && (cmd !== 'setup' && cmd !== 'help' && cmd !== 'version')) {
+    console.log("\x1b[31;1m%s\x1b[0m", "\u2718 .env file not found! Please run 'amifree setup' to fix this error. Line 32");
+    return;
+  }
+  
+  /**
+   * TODO: If user has not yet set credentials.json, terminate the application.  
+   */
+  if (!fs.existsSync(path.join(__dirname, "credentials.json")) && (cmd !== 'setup' && cmd !== 'help' && cmd !== 'version')) {
+    console.log("\x1b[31;1m%s\x1b[0m", "\u2718 credentials.json file not found! Please run 'amifree setup' to fix this error.");
+    return;
   }
 
   switch (cmd) {
