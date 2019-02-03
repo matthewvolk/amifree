4b5172e34352865cdc6b5acb8bc339fab27e4e16
Matthew Volk <volkmattj@gmail.com>
2019-02-02 20:08:37 -0800

    successfully implement amifree setup command

--- cmds/setup.js
+++ cmds/setup.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout
@@ -27,7 +28,49 @@ const getCredentialsJson = () => {
   readline.question("\nDrag the downloaded credentials JSON file into this terminal to get the path and then press ENTER:\n", (path) => {
     console.log(`Importing Credentials.json from: ${path}`);
     readline.close();
-    // TODO: Validate Credentials.json
+    validateCredentialsJson(path);
+  });
+}
+
+const validateCredentialsJson = (path) => {
+  var obj;
+  fs.readFile(path, 'utf8', function (err, data) {
+    if (err) {
+      console.log('\x1b[31;1m\u2718 ERROR: Path does not lead to a valid JSON file:\x1b[0m', err.path);
+      return;
+    }
+
+    obj = JSON.parse(data);
+
+    /**
+     * TODO:
+     * Better file validation
+     */
+    if (
+      !obj.installed || 
+      !obj.installed.client_id ||
+      !obj.installed.project_id ||
+      !obj.installed.auth_uri ||
+      !obj.installed.token_uri ||
+      !obj.installed.auth_provider_x509_cert_url ||
+      !obj.installed.client_secret ||
+      !obj.installed.redirect_uris
+      ) {
+      console.log(`\x1b[31;1m\u2718 ERROR: File contains valid JSON, but might be the Incorrect JSON File: ${path}\x1b[0m`);
+      return;
+    }
+
+    // If JSON is valid, create credentials.json in root directory
+    json = JSON.stringify(obj);
+    fs.writeFile('./credentials.json', json, (err) => {
+      if (err) {
+        console.log('\x1b[31;1m\u2718 ERROR: credentials JSON file could not be written.\x1b[0m')
+      }
+      if (!err) {
+        console.log('\x1b[92;1m\u2714 credentials.json file created!\x1b[0m');
+        require('../cmds/today');
+      }
+    });
   });
 }
 
