d146fd83394976aaa9b5166064436c3df03254e7
Matthew Volk <volkmattj@gmail.com>
2019-02-02 20:44:25 -0800

    create tutorial for OAuth

--- cmds/setup.js
+++ cmds/setup.js
@@ -13,22 +13,16 @@ const instructions = `\x1b[1m
  \\__,_|_| |_| |_|_|_| |_|  \\___|\\___(_) 
 \x1b[0m
 
-\x1b[1m1. Open a web browser and navigate to: \x1b[0m
-\x1b[34;4mhttps://console.developers.google.com/\x1b[0m
-\x1b[1m2. Follow steps 1 - 3 under "Get started": \x1b[0m
-\x1b[34;4mhttps://support.google.com/cloud/answer/3465889?hl=en\x1b[0m
-\x1b[1m3. Navigate to the Credentials page, select Create credentials.\x1b[0m
-\x1b[1m4. Select OAuth client ID.\x1b[0m
-\x1b[1m5. Supply the requested information.\x1b[0m
-\x1b[1m6. Click Save to return to the Credentials screen.\x1b[0m
-\x1b[1m7. Select Other for the Application type.\x1b[0m
-\x1b[1m8. Download the JSON file containing your credentials.\x1b[0m`
+Welcome! This application uses Google OAuth2 to read your Google Calendar events.
+For a tutorial explaining exactly how to download your Google OAuth2 Credentials
+for use specifically with this application, please visit:
+{{ TODO: CREATE TUTORIAL AND LINK IT AS A README FROM GITHUB }}`
 
 const getCredentialsJson = () => {
   readline.question("\nDrag the downloaded credentials JSON file into this terminal to get the path and then press ENTER:\n", (path) => {
     console.log(`Importing Credentials.json from: ${path}`);
     readline.close();
-    validateCredentialsJson(path);
+    validateCredentialsJson(path.trim());
   });
 }
 
