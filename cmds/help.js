851e2f8bf2803aa89c8046af533b76139e33c44b
Matthew Volk <volkmattj@gmail.com>
2019-02-04 18:55:03 -0800

    update help command

--- cmds/help.js
+++ cmds/help.js
@@ -1,10 +1,10 @@
 const help = `
-\x1b[0mUsage: amifree [options]
+\x1b[0mUsage: \x1b[36;1mamifree [options]\x1b[0m
 
 Options:
+  \x1b[36;1msetup\x1b[0m                    One time CLI setup and configuration process.
   \x1b[36;1mtoday\x1b[0m                    List Google Calendar availability for today.
   \x1b[36;1mtomorrow\x1b[0m                 List Google Calendar availability for tomorrow.
-  \x1b[36;1mconfig\x1b[0m                    One time CLI setup and configuration process.
   \x1b[36;1minfo\x1b[0m                     Print out information about your configuration.
   \x1b[36;1mversion, -v, --version\x1b[0m   Show package version.
   \x1b[36;1mhelp, -h, --help\x1b[0m         Display this help menu.
@@ -13,7 +13,7 @@ Examples:
   \x1b[36;1mamifree today\x1b[0m            # Displays available timeslots on your calendar for current day
   \x1b[36;1mamifree tomorrow\x1b[0m         # Displays availabel timelines on your calendar for tomorrow 
 
-Documentation can be found at https://github.com/matthewvolk/amifree
+Documentation can be found at \x1b[36;1mhttps://github.com/matthewvolk/amifree\x1b[0m 
 `
 module.exports = () => {
   console.log(help);
