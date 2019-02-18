22facb0506ba17bf277170ba39494e3f691c63be
Matthew Volk <volkmattj@gmail.com>
2019-02-17 20:30:57 -0800

    add todo

--- cmds/info.js
+++ cmds/info.js
@@ -3,6 +3,11 @@ const START_OF_TODAY = new Date();
 const END_OF_TODAY = new Date();
       END_OF_TODAY.setHours(23,59,59,999);
 
+/**
+ * TODO: 
+ * + Add time zone
+ */
+
 const info = `\n\x1b[92;1m\u2714 Reading from the Google Calendar of\x1b[0m: ${process.env.CALENDAR_ID}
 \x1b[92;1m\u2714 The current date is\x1b[0m: ${START_OF_TODAY.toDateString()} 
 `
